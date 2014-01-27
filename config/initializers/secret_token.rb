a5ac6fcfad485e20a91e91d760bc9924c52fdafc
noble gold <noble@nobles-MacBook-Pro.local>
2014-01-27 13:50:15 -0500

    all my blog files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '943da74c18b558a274832b99725d3405041374781bd615b8bb495d4a7355f29f5b1876cc45cee714399f3353153177ebed53d58d15bad9eb511395ea4d5c15ca'
